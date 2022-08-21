df8e4256d542a0837faddf97eb878c77b75c81ec
Marcos Petrungaro <marcos.petrungaro@gmail.com>
2022-08-21 12:12:26 -0300

    Sorting cities combo

--- src/services/cities.service.js
+++ src/services/cities.service.js
@@ -6,5 +6,6 @@ const api = axios.create({ baseURL })
 
 export async function getCities() {
   const { data: cities } = await api.get('/cities')
+  cities.sort((a, b) => a.name.localeCompare(b.name))
   return cities
 }
